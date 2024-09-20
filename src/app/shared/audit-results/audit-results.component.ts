9d70db34097f238e02c3ef3b55662879e6f6e069
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-09-20 15:47:25 -0600

    feat: generate structure from the project

---
+++ src/app/shared/audit-results/audit-results.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-audit-results',
+  standalone: true,
+  imports: [],
+  templateUrl: './audit-results.component.html',
+  styleUrl: './audit-results.component.css'
+})
+export class AuditResultsComponent {
+
+}
---
+++ src/app/shared/url-form/url-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-url-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './url-form.component.html',
+  styleUrl: './url-form.component.css'
+})
+export class UrlFormComponent {
+
+}
