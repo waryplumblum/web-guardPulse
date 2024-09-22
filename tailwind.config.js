a343a180b874d9c1a913b84a89b1fb3ace0c3468
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-09-22 14:57:41 -0600

    feat: css and Html new design

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        custom: {
+          'tan': '#DBB58D',
+          'gold': '#DBBA44',
+          'light-gray': '#DDD3D1',
+          'bronze': '#DB9244',
+          'orange': '#DBA844',
+          'dark-orange': '#DB7844',
+        },
+      },
+    },
   },
   plugins: [],
 }
