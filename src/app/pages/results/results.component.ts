3b1824e66c949ba65213f7c06a92557c29309b4d
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-09-27 13:34:11 -0600

    Reapply "feat: add new features"
    This reverts commit 87c4d6ac9570ce67b9e262a44c4c8679adfd890c.

--- src/app/pages/results/results.component.ts
+++ src/app/pages/results/results.component.ts
@@ -1,31 +1,40 @@
 import { Component, signal, inject } from '@angular/core';
 import { AuditInterface } from '../../interfaces/audit.interface';
 import { AuditsService } from '../../services/audit.service';
-import { FormsModule } from '@angular/forms';
+import { UrlFormComponent } from '../../shared/url-form/url-form.component';
+import { AuditResultComponent } from '../../shared/audit-results/audit-results.component';
+import { CommonModule } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-results',
   standalone: true,
-  imports: [FormsModule], // Asegúrate de importar FormsModule
+  imports: [UrlFormComponent,AuditResultComponent,CommonModule], // Asegúrate de importar FormsModule
   templateUrl: './results.component.html',
   styleUrls: ['./results.component.css']
 })
 export default class ResultsComponent {
-  url = signal(''); // Usamos signal para el estado de la URL
   auditResult = signal<AuditInterface>({} as AuditInterface);  // Signal para el resultado de la auditoría
+  isLoading = signal(false);
 
-  // Inyección de AuditsService usando inject
+  // Inyección del servicio de auditorías
   private auditsService = inject(AuditsService);
 
-  onSubmit() {
-    this.auditsService.auditUrl(this.url()).subscribe({
+  onUrlSubmit(url: string) {
+    this.isLoading.set(true);
+    console.log("Iniciando auditoría, isLoading:", this.isLoading());
+    this.auditsService.auditUrl(url).subscribe({
       next: (result) => {
-        this.auditResult.set(result); // Actualiza el resultado de la auditoría
-        console.log('Audit result:', this.auditResult()); // Accede al valor de la signal
+        console.log("Resultados de la auditoría:", result);
+        this.auditResult.set(result); // Actualiza los resultados
+        this.isLoading.set(false);
       },
       error: (error) => {
-        console.error('Error during audit:', error);
+        console.error('Error durante la auditoría:', error);
+        this.isLoading.set(false);
       }
     });
   }
+
 }
