9918e2d7f1e71472b1f3b2634daa822378b2832c
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-10-03 00:52:55 -0600

    feat: ctegory design html and ts

--- src/app/shared/audit-results/audit-results.component.ts
+++ src/app/shared/audit-results/audit-results.component.ts
@@ -15,6 +15,40 @@ export class AuditResultComponent implements OnChanges {
   @Input() auditResult: AuditResultDetails | null = null;
   auditResultSignal = signal<AuditResultDetails | null>(null);
 
+  // Computed Signal para construir dinámicamente las métricas basadas en el 'auditResult'
+  metrics = computed(() => {
+    if (!this.auditResultSignal()) return [];
+
+    const auditResult = this.auditResultSignal()!;
+
+    return [
+      {
+        label: 'SEO',
+        score: auditResult.seo,
+        bgClass: auditResult.seo === 100 ? 'bg-green-100' : 'bg-yellow-100',
+        textColorClass: auditResult.seo === 100 ? 'text-green-600' : 'text-yellow-600',
+        positiveMessage: '¡Excelente! Tu sitio web está totalmente optimizado para SEO.',
+        negativeMessage: 'Mejorar la optimización de meta etiquetas y enlaces internos.'
+      },
+      {
+        label: 'Accesibilidad',
+        score: auditResult.accessibility,
+        bgClass: auditResult.accessibility === 100 ? 'bg-green-100' : 'bg-yellow-100',
+        textColorClass: auditResult.accessibility === 100 ? 'text-green-600' : 'text-yellow-600',
+        positiveMessage: '¡Perfecto! Tu sitio cumple con todas las buenas prácticas de accesibilidad.',
+        negativeMessage: 'Asegúrate de que todas las imágenes tengan texto alternativo y verifica el contraste de color.'
+      },
+      {
+        label: 'Rendimiento',
+        score: auditResult.performance,
+        bgClass: auditResult.performance >= 90 ? 'bg-green-100' : 'bg-red-100',
+        textColorClass: auditResult.performance >= 90 ? 'text-green-600' : 'text-red-600',
+        positiveMessage: 'Tu sitio web tiene un rendimiento óptimo.',
+        negativeMessage: 'Optimiza el tiempo de carga de los recursos y mejora el tiempo de respuesta del servidor.'
+      }
+    ];
+  });
+
   // Mapa de categorías a métricas
   categories = {
     generalSettings:{
@@ -189,39 +223,6 @@ export class AuditResultComponent implements OnChanges {
     },
   };
 
-  // Computed Signal para construir dinámicamente las métricas basadas en el 'auditResult'
-  metrics = computed(() => {
-    if (!this.auditResultSignal()) return [];
-
-    const auditResult = this.auditResultSignal()!;
-
-    return [
-      {
-        label: 'SEO',
-        score: auditResult.seo,
-        bgClass: auditResult.seo === 100 ? 'bg-green-100' : 'bg-yellow-100',
-        textColorClass: auditResult.seo === 100 ? 'text-green-600' : 'text-yellow-600',
-        positiveMessage: '¡Excelente! Tu sitio web está totalmente optimizado para SEO.',
-        negativeMessage: 'Mejorar la optimización de meta etiquetas y enlaces internos.'
-      },
-      {
-        label: 'Accesibilidad',
-        score: auditResult.accessibility,
-        bgClass: auditResult.accessibility === 100 ? 'bg-green-100' : 'bg-yellow-100',
-        textColorClass: auditResult.accessibility === 100 ? 'text-green-600' : 'text-yellow-600',
-        positiveMessage: '¡Perfecto! Tu sitio cumple con todas las buenas prácticas de accesibilidad.',
-        negativeMessage: 'Asegúrate de que todas las imágenes tengan texto alternativo y verifica el contraste de color.'
-      },
-      {
-        label: 'Rendimiento',
-        score: auditResult.performance,
-        bgClass: auditResult.performance >= 90 ? 'bg-green-100' : 'bg-red-100',
-        textColorClass: auditResult.performance >= 90 ? 'text-green-600' : 'text-red-600',
-        positiveMessage: 'Tu sitio web tiene un rendimiento óptimo.',
-        negativeMessage: 'Optimiza el tiempo de carga de los recursos y mejora el tiempo de respuesta del servidor.'
-      }
-    ];
-  });
 
   ngOnChanges() {
     if (this.auditResult) {
@@ -230,24 +231,3 @@ export class AuditResultComponent implements OnChanges {
   }
 
 }
-
-//   reportDetails = [
-//     { key: 'first-contentful-paint', label: 'First Contentful Paint' },
-//     { key: 'largest-contentful-paint', label: 'Largest Contentful Paint' },
-//     { key: 'speed-index', label: 'Speed Index' },
-//     { key: 'server-response-time', label: 'Tiempo de Respuesta del Servidor' },
-//     { key: 'total-blocking-time', label: 'Tiempo Total de Bloqueo' },
-//     { key: 'cumulative-layout-shift', label: 'Cumulative Layout Shift' },
-//   ];
-
-//   seoDetails = [
-//     { key: 'document-title', label: 'Título del Documento' },
-//     { key: 'meta-description', label: 'Descripción Meta' },
-//     { key: 'robots-txt', label: 'Estado del Robots.txt' },
-//   ];
-
-//   accessibilityDetails = [
-//     { key: 'color-contrast', label: 'Contraste de Color' },
-//     { key: 'image-alt', label: 'Texto Alternativo en Imágenes' },
-//     { key: 'heading-order', label: 'Orden de Encabezados' },
-//   ];
